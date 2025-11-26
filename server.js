763faa6308f7c24549ac3aeba16ce91a4e484484
Sunandha Mani Vajrapu <vajrapusunandhamani@gmail.com>
2025-11-26 16:19:15 +0530

    Fix CORS for Vercel

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-dotenv.config();   
+dotenv.config();
 
 import connectDB from './config/db.js';
 import taskRoutes from './routes/taskRoutes.js';
@@ -10,7 +10,17 @@ import authRoutes from "./routes/authRoutes.js";
 connectDB();
 
 const app = express();
-app.use(cors());
+
+// ⭐ IMPORTANT: Correct CORS for Vercel + Localhost
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://todo-frontend-z5h5-bmu72om10-sunandha165s-projects.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -20,5 +30,3 @@ app.get('/', (req, res) => res.send('Task API running'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-//mongodb+srv://vajrapusunandhamani_db_user:XtEam0mBzQ8HrFFZ@cluster0.cx2uydl.mongodb.net/?appName=Cluster0
